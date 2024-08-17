e8deda4fa7f01997e6d4a809d15daf475f0b1816
BtJ-gogo <ymawro@gmail.com>
2024-08-17 16:41:31 +0900

    feat: add code

--- mean-variance-standared-deviation-calculator/mean_var_std.py
+++ mean-variance-standared-deviation-calculator/mean_var_std.py
@@ -1,1 +1,29 @@
+import numpy as np
 
+def calculate(list):
+    calculations = {}
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
+    
+    data_array = np.array([list[0:3], list[3:6], list[6:9]])
+
+    # cal mean
+    calculations['mean'] = [np.mean(data_array, axis=0).tolist(), np.mean(data_array, axis=1).tolist(), np.mean(data_array)]
+    
+    #cal variance
+    calculations['variance'] = [np.var(data_array, axis=0).tolist(), np.var(data_array, axis=1).tolist(), np.var(data_array)]
+
+    #cal std
+    calculations['standard deviation'] = [np.std(data_array, axis=0).tolist(), np.std(data_array, axis=1).tolist(), np.std(data_array)]
+
+    #cal max
+    calculations['max'] = [np.max(data_array, axis=0).tolist(), np.max(data_array, axis=1).tolist(), np.max(data_array)]
+
+    #cal min
+    calculations['min'] = [np.min(data_array, axis=0).tolist(), np.min(data_array, axis=1).tolist(), np.min(data_array)]
+
+    #cal sum
+    calculations['sum'] = [np.sum(data_array, axis=0).tolist(), np.sum(data_array, axis=1).tolist(), np.sum(data_array)]
+
+    
+    return calculations
